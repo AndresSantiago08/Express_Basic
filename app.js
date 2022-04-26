85a43a2bcac46e3366b7381e271095201afb83cb
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-04-26 11:24:46 -0500

    Regresando un bojeto y recibiendo parámetros por medio de la url

--- app.js
+++ app.js
@@ -5,14 +5,28 @@ const app = express();
 // Puesrto para ve la app localhost:5000
 const port = 5000;
 
-// Path inicial, respondera a la url localhost
+// Path inicial, respondera a la url localhost:5000
 app.get('/', (req, res) => {
     res.send("Hola mundo!")
 })
 
+// Ruta adicional
 // localhost:5000/launchx
 app.get('/launchx', (req, res) => {
     res.send("Bienvenidos a Launch X!")
+})
+
+// Regresando un objeto
+// localhost:5000/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: "Explorer", msg: "Hola"};
+    res.send(explorer)
+})
+
+// Query Params: Recibir parámetros por la url
+// localhost:5000/explorers/and
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params)
 })
 
 // Inicializando app
