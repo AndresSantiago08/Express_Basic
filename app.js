ab4274117b1bdb5cc7561db7e2d3f0c9869390b9
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-04-26 11:05:53 -0500

    Agregando nueva ruta a la app

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
     res.send("Hola mundo!")
 })
 
+// localhost:5000/launchx
+app.get('/launchx', (req, res) => {
+    res.send("Bienvenidos a Launch X!")
+})
+
 // Inicializando app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
